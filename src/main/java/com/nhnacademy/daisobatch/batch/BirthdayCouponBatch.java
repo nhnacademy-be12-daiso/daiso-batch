14f6741f4bdb23cd9c4e1aa15048de3c11c8d9e6
hanum <dlgksdma15@naver.com>
2025-12-03 15:24:57 +0900

    [feat]: batch-server @StepScope 추가

--- src/main/java/com/nhnacademy/daisobatch/batch/BirthdayCouponBatch.java
+++ src/main/java/com/nhnacademy/daisobatch/batch/BirthdayCouponBatch.java
@@ -9,6 +9,7 @@ import com.nhnacademy.daisobatch.repository.UserCouponRepository;
 import com.nhnacademy.daisobatch.type.CouponStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -32,6 +33,8 @@ public class BirthdayCouponBatch {
     private final CouponPolicyRepository couponPolicyRepository;
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
+    private CouponPolicy birthdayPolicy;  // ← 캐싱
+
 
     public BirthdayCouponBatch(UserServiceClient userServiceClient,
                                UserCouponRepository couponRepository,
@@ -52,21 +55,23 @@ public class BirthdayCouponBatch {
                 .build();
     }
     // 2. Step 정의
+    // 서버가 cpu:2~4코어, 메모리: 4~8GB, DB커넥션 풀:10~20개 일떄
+    // chunk = 100일 때 10,000명당 1분 30초 걸림. 회원이 12만명일때까지는 2분내외로 처리 가능!
     @Bean
     public Step birthdayCouponStep(){
         return new StepBuilder("birthdayCouponStep",jobRepository)
-                .<BirthdayUserDto, UserCoupon>chunk(10, transactionManager) // 10명씩 끊어서 처리
-                .reader(birthdayUserReader())
-                .processor(birthdayUserProcessor())
-                .writer(birthdayUserWriter())
+                .<BirthdayUserDto, UserCoupon>chunk(100, transactionManager) // 100명씩 끊어서 처리, 1만명이명 1분 30초
+                .reader(birthdayUserReader()) // 100명 읽기
+                .processor(birthdayUserProcessor()) // 100명 변환
+                .writer(birthdayUserWriter()) // 100명 저장 + 커밋
                 .build();
     }
     // 3. Reader
     @Bean
+    @StepScope
     public ItemReader<BirthdayUserDto> birthdayUserReader(){
         // 이번 달 구하기
         int currentMonth = LocalDate.now().getMonthValue();
-
         // User 서버에서 생일자 목록 조회
         List<BirthdayUserDto> users = userServiceClient.getBirthdayUsers(currentMonth);
         // 데이터가 없으면 빈 리스트 반환
@@ -79,12 +84,23 @@ public class BirthdayCouponBatch {
     // 4. Processor: DTO -> Entity 변환
     @Bean
     public ItemProcessor<BirthdayUserDto, UserCoupon> birthdayUserProcessor(){
+        if(birthdayPolicy == null){
+            birthdayPolicy = couponPolicyRepository.findById(4L)
+                    .orElseThrow(() -> new IllegalStateException("생일 쿠폰 정책이 없습니다."));
+        }
+
+
         return item -> {
             // A.생일 쿠폰 정책 조회 (고정된 ID나 코드로 조회한다고 가정)
             // 실제로는 매번 조회하면 성능 이슈가 있으므로 캐싱하거나, StepExecutionListener 등으로 미리 로딩하는 것이 좋습니다.
-            // 여기서는 예시로 ID가 1L인 정책을 생일 쿠폰 정채깅라 가정합니다.
-            CouponPolicy policy = couponPolicyRepository.findById(4L)
-                    .orElseThrow(() -> new IllegalStateException("생일 쿠폰 정책(ID:4)이 존재하지 않습니다."));
+
+            // 여기서는 예시로 ID가 1L인 정책을 생일 쿠폰 정책이라 가정합니다.
+
+            boolean alreadyHas = couponRepository.existsByUserIdAndCouponPolicy_CouponPolicyId(
+                    item.getUserCreatedId(), 4L);
+            if(alreadyHas){
+                return null; // null 반환시 Writer에서 스킵.
+            }
 
             // B. 유효기간 설정 (이번 달 1일 ~ 이번 달 마지막 날)
             LocalDate now = LocalDate.now();
@@ -96,7 +112,7 @@ public class BirthdayCouponBatch {
 
             // C. 엔티티 생성 및 반환
             return UserCoupon.builder()
-                    .couponPolicy(policy)
+                    .couponPolicy(birthdayPolicy)
                     .userId(item.getUserCreatedId())
                     .status(CouponStatus.ISSUED)
                     .issuedAt(issuedAt)
